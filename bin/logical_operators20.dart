a992ba302cf56a5a231ab36a217d1933c6a6ca98
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-02-25 10:51:31 +0500

    logical20

--- bin/logical_operators20.dart
+++ bin/logical_operators20.dart
@@ -10,4 +10,20 @@ Five number consisting of one and zero is given (the number starts at once).
         bool: answer
 */
 
-void main() {}
+bool func(int a) {
+  int x1 = a % 10;
+  x1 = a ~/ 10;
+  int x2 = a % 10;
+  x2 = a ~/ 10;
+  int x3 = a % 10;
+  x3 = a ~/ 10;
+  int x4 = a % 10;
+  int x5 = a ~/ 10;
+  int x6 = x1 + x2 + x3 + x4 + x5;
+  bool answer = x6 >= 2;
+  return answer;
+}
+
+void main() {
+  print(func(10100));
+}
