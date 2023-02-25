6db0e1ad2c77a3cf51e748837c6878ff795ce8c4
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-02-25 10:49:23 +0500

    logical17

--- bin/logical_operators17.dart
+++ bin/logical_operators17.dart
@@ -7,5 +7,21 @@ Given a five-digit integer a,  check the following statement "All digits of the 
     Returns:
         bool: answer
 */
+bool func(int a) {
+  int x1 = a % 10;
+  x1 = x1 ~/ 10;
+  int x2 = a % 100;
+  x2 = x2 ~/ 10;
+  int x3 = a % 1000;
+  x3 = x3 ~/ 100;
+  int x4 = a % 10000;
+  x4 = x4 ~/ 1000;
+  int x5 = a % 100000;
+  x5 = x5 ~/ 10000;
+  bool answer = x1 > x2 && x2 > x3 && x3 > x4 && x4 > x5;
+  return answer;
+}
 
-void main() {}
+void main() {
+  print(func(12745));
+}
