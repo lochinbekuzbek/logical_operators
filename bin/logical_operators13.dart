c1b9eb910b05a64fde626a7caa36b0e0f4ba1c66
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-02-25 10:46:23 +0500

    logical13

--- bin/logical_operators13.dart
+++ bin/logical_operators13.dart
@@ -8,4 +8,13 @@ Given a two-digit integer a,  check the following statement "All digits sum is e
         bool: answer
 */
 
-void main() {}
+bool func(int a) {
+  int b = a % 10;
+  a ~/= 10;
+  bool answer = a % 2 == 0 && b % 2 == 0;
+  return answer;
+}
+
+void main() {
+  print(func(68));
+}
