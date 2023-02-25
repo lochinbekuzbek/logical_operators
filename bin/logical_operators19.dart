16244d9a11f45904cd64d0d1da5887dc176ab38d
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-02-25 10:50:45 +0500

    logical19

--- bin/logical_operators19.dart
+++ bin/logical_operators19.dart
@@ -1,12 +1,20 @@
 /*
-Create function called func
 Create a function argument called x of type int
 Given a three digit integer x, return true if x is palindrome integer.
-  An integer is a palindrome when it reads the same backward as forward.
+  An integCreate function called func
+er is a palindrome when it reads the same backward as forward.
   Args:
       x(int): parameter x
   Returns:
       bool: answer
 */
+bool func(int x) {
+  int a = x ~/ 100;
+  int b = x % 10;
+  bool answer = a == b;
+  return answer;
+}
 
-void main() {}
+void main() {
+  print(func(564));
+}
